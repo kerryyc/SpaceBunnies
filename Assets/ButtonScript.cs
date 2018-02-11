dbaebd260cd3f63767cba4a27ede4b1e88e8049c
Kerry Cheng <kerryc635@gmail.com>
2018-02-10 17:35:54 -0800

    Added start at level 1, quit, and reload current scene in script to use for buttons. Main menu buttons now work

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void StartGame() {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
