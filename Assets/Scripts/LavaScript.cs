a3dbc3d8048749c1111e4ef8151c3c6567feff46
Kerry Cheng <kerryc635@gmail.com>
2018-02-19 16:17:06 -0800

    changed controls text in pause menu. Added timer for lava

--- Assets/Scripts/LavaScript.cs
+++ Assets/Scripts/LavaScript.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class LavaScript : MonoBehaviour {
 
     private Rigidbody2D rb2d;
+    private float time;
 
     public float speed = 1f;
+    public float cooldown = 10f;
 
     void Awake() {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    void Start() {
+        cooldown = cooldown + Time.time;
+    }
+
     void FixedUpdate() {
-        rb2d.velocity = new Vector2(0, speed);
+        if (Time.time > cooldown)
+            rb2d.velocity = new Vector2(0, speed);
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
